43bc50824e9205c0061374e2458fbca6da883130
David Costa <david.mrcs@gmail.com>
2019-01-13 05:06:36 -0300

    novo

---
+++ coursera/semana4/nimpares.py
@@ -0,0 +1,10 @@
+n = int(input("Digite o valor de n: "))
+for i in range(1, n+n, 2):
+   print(i)
+
+'''
+a função range(start, stop, step)
+Start: número inicial da sequência.
+Stop: até este número, mas não o inclui na sequência.
+Step: diferença entre cada número na sequência.
+'''
